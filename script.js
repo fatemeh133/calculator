e0de25b354e937b127b9e13e20c90e703b538d6c
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2023-11-18 12:30:59 +0330

    fix show numbers

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-console.log(document.querySelector(".cal-head").innerHTML);
 let calHead = document.querySelector(".cal-head");
 let btnNumbers = document.querySelectorAll(".btn-number");
+let btnDEL = document.querySelectorAll(".btn-del");
+
+//add number in calculator head
 btnNumbers.forEach(function btnLoop(btnNumber) {
   btnNumber.addEventListener(`click`, function name(params) {
-    calHead.innerHTML = btnNumber.innerHTML;
+    if (calHead.textContent == 0) {
+      calHead.textContent = btnNumber.textContent;
+    } else {
+      calHead.textContent += btnNumber.textContent;
+    }
   });
 });
