399075f54dad5fe7984cf11d49c8ed1dc2d4292d
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2023-11-18 16:57:47 +0330

    operation

--- script.js
+++ script.js
@@ -3,30 +3,43 @@ let btnNumbers = document.querySelectorAll(".btn-number");
 let btnDEL = document.querySelector(".btn-del");
 let btnRES = document.querySelector(".btn-reset");
 let btnEqual = document.querySelector(".btn-equal");
+let btnOperator = document.querySelectorAll(".btn-Operator");
 
-//add number in calculator head
-btnNumbers.forEach(function btnLoop(btnNumber) {
-  btnNumber.addEventListener(`click`, function addNumber() {
-    if (calHead.textContent == 0) {
+// Add number in calculator head
+btnNumbers.forEach(function (btnNumber) {
+  btnNumber.addEventListener("click", function addNumber() {
+    if (calHead.textContent === "0") {
       calHead.textContent = btnNumber.textContent;
     } else {
       calHead.textContent += btnNumber.textContent;
     }
-    if (isNaN(Number(calHead.textContent))) {
-      console.log("hi");
-    }
   });
 });
 
-btnDEL.addEventListener(`click`, function DELETE() {
+// Delete last character
+btnDEL.addEventListener("click", function DELETE() {
   let cutStr = calHead.textContent.slice(0, -1);
-  if (cutStr) {
-    calHead.textContent = cutStr;
-  } else calHead.textContent = 0;
+  calHead.textContent = cutStr || "0";
 });
 
-btnRES.addEventListener(`click`, function RESET() {
-  calHead.textContent = 0;
+// Reset calculator
+btnRES.addEventListener("click", function RESET() {
+  calHead.textContent = "0";
 });
 
-btnEqual.addEventListener(`click`, function RESET() {});
+// Perform calculation when "=" is clicked
+btnEqual.addEventListener("click", function EQUAL() {
+  // Replace "x" with "*"
+  let expression = calHead.textContent.replace(/x/g, "*");
+
+
+  // Use eval to evaluate the expression and update calHead
+  calHead.textContent = eval(expression);
+});
+
+// Add operators to the calculator head
+btnOperator.forEach(function (btnOperator) {
+  btnOperator.addEventListener("click", function addOperator() {
+    calHead.textContent += btnOperator.textContent;
+  });
+});
