5c11531ef2a83b9aed07ce49677b3d10d0dcaa22
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2023-11-18 14:45:08 +0330

    add delete and reset

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 let calHead = document.querySelector(".cal-head");
 let btnNumbers = document.querySelectorAll(".btn-number");
-let btnDEL = document.querySelectorAll(".btn-del");
+let btnDEL = document.querySelector(".btn-del");
+let btnRES = document.querySelector(".btn-reset");
+let btnEqual = document.querySelector(".btn-equal");
 
 //add number in calculator head
 btnNumbers.forEach(function btnLoop(btnNumber) {
-  btnNumber.addEventListener(`click`, function name(params) {
+  btnNumber.addEventListener(`click`, function addNumber() {
     if (calHead.textContent == 0) {
       calHead.textContent = btnNumber.textContent;
     } else {
       calHead.textContent += btnNumber.textContent;
     }
+    if (isNaN(Number(calHead.textContent))) {
+      console.log("hi");
+    }
   });
 });
+
+btnDEL.addEventListener(`click`, function DELETE() {
+  let cutStr = calHead.textContent.slice(0, -1);
+  if (cutStr) {
+    calHead.textContent = cutStr;
+  } else calHead.textContent = 0;
+});
+
+btnRES.addEventListener(`click`, function RESET() {
+  calHead.textContent = 0;
+});
+
+btnEqual.addEventListener(`click`, function RESET() {});
